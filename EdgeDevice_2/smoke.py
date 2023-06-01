b56f4411aac9a2d69020e6687dc87985f61c1f5b
Mohammad Chohan <mohammadchohan6f@gmail.com>
2023-06-01 18:16:17 +0900

    remove development code

--- EdgeDevice_2/smoke.py
+++ EdgeDevice_2/smoke.py
@@ -1,24 +1,14 @@
-import random
-import time
-
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import serial
-
-in_production = False
 
 thingsboard_topic = "v1/devices/me/telemetry"
 smoke_access_token = "l5rnGguS4Yje8axG8wGb"
 thingsboard_key = "smoke"
 
 smoke_topic = "smoke"
-
 buzzer_command_topic = "SmokeBuzzer"
 smoke_led_command_topic = "SmokeLed"
-
-
-def generate_value():
-    return random.randrange(40, 200)
 
 
 def generate_payload(key, value):
@@ -46,8 +36,7 @@ def on_message(client, userdata, msg):
     print("Received message: " + msg.payload.decode())
     print("From: " + msg.topic)
 
-    if in_production:
-        smoke_arduino.write(msg.payload)
+    smoke_arduino.write(msg.payload)
 
 
 mqtt_client = mqtt.Client()
@@ -57,14 +46,9 @@ mqtt_client.on_message = on_message
 mqtt_client.connect("test.mosquitto.org", 1883, 60)
 mqtt_client.loop_start()
 
-if in_production:
-    smoke_arduino = serial.Serial('/dev/ttyS1', 9600)
+smoke_arduino = serial.Serial('/dev/ttyS1', 9600)
 
-    while True:
-        if smoke_arduino.in_waiting:
-            smoke_data = smoke_arduino.readline().strip()
-            publish_value(smoke_data)
-else:
-    while True:
-        publish_value(generate_value())
-        time.sleep(2)
+while True:
+    if smoke_arduino.in_waiting:
+        smoke_data = smoke_arduino.readline().strip()
+        publish_value(smoke_data)
