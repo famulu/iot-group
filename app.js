afa325145b38f24cfc9f72907982725dac77c7a2
Mohammad Chohan <mohammadchohan6f@gmail.com>
2023-06-01 13:59:45 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+import 'dotenv/config';
+import express from 'express';
+import {
+  InteractionType,
+  InteractionResponseType,
+} from 'discord-interactions';
+import { VerifyDiscordRequest, getRandomEmoji } from './utils.js';
+import { connect } from "mqtt"  // import connect from mqtt
+import { commands } from './commands.js';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Parse request body and verifies incoming requests using discord-interactions package
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+
+const client = connect('mqtt://test.mosquitto.org') // create a client
+
+/**
+ * Interactions endpoint URL where Discord will send HTTP requests
+ */
+app.post('/interactions', async function (req, res) {
+  // Interaction type and data
+  const { type, data } = req.body;
+
+  /**
+   * Handle verification requests
+   */
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  /**
+   * Handle slash command requests
+   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+   */
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    // "test" command
+    if (name === 'test') {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: 'hello world ' + getRandomEmoji(),
+        },
+      });
+    }
+
+    // "command" command
+    if (name === 'command') {
+      const command = req.body.data.options[0].value;
+      const { topic, payload } = commands[command]
+      client.publish(topic, String(payload))
+
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `Message Delivered`
+        },
+      });
+    }
+  }
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
---
+++ commands.js
@@ -0,0 +1,85 @@
+import 'dotenv/config';
+import { InstallGlobalCommands } from './utils.js';
+
+export const commands = {
+  "Buzzer ON": {
+    payload: 3,
+    topic: "SmokeBuzzer"
+  },
+  "Buzzer OFF": {
+    payload: 4,
+    topic: "SmokeBuzzer"
+  },
+  "Fan ON": {
+    payload: 3,
+    topic: "ThermoFan"
+  },
+  "Fan OFF": {
+    payload: 4,
+    topic: "ThermoFan"
+  },
+  "Temperature LED ON": {
+    payload: 1,
+    topic: "ThermoLed"
+  },
+  "Temperature LED OFF": {
+    payload: 2,
+    topic: "ThermoLed"
+  },
+  "Smoke LED ON": {
+    payload: 1,
+    topic: "SmokeLed"
+  },
+  "Smoke LED OFF": {
+    payload: 2,
+    topic: "SmokeLed"
+  },
+  "Motion LED ON": {
+    payload: 1,
+    topic: "MotionLed"
+  },
+  "Motion LED OFF": {
+    payload: 2,
+    topic: "MotionLed"
+  }
+}
+
+// Get the game choices from game.js
+function createCommandChoices() {
+  const commandChoices = [];
+
+  for (let choice of Object.keys(commands)) {
+    commandChoices.push({
+      name: choice,
+      value: choice,
+    });
+  }
+  return commandChoices;
+}
+
+// Simple test command
+const TEST_COMMAND = {
+  name: 'test',
+  description: 'Basic command',
+  type: 1,
+};
+
+// Command containing options
+const COMMAND_COMMAND = {
+  name: 'command',
+  description: 'Send a command to an actuator',
+  options: [
+    {
+      type: 3,
+      name: 'command',
+      description: 'Pick your command',
+      required: true,
+      choices: createCommandChoices(),
+    },
+  ],
+  type: 1,
+};
+
+const ALL_COMMANDS = [TEST_COMMAND, COMMAND_COMMAND];
+
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
